923e8596a91a9b63ea04ba725af56d94ee215a46
Doris <guerrerodoris08@gmail.com>
2023-09-18 17:08:08 -0500

    Gallery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,29 @@
-import Header from "../components/header/Header";
+import { ChakraProvider, CSSReset, extendTheme, Box } from '@chakra-ui/react';
+/* ---------------------------------------------------------------------------- */
+import Header from '../components/header/Header';
+import Gallery from '../components/gallery/Gallery';
+/* ---------------------------------------------------------------------------- */
+// Aqui definimos el tema de Chakra UI si es necesario.
+const theme = extendTheme({
 
+});
+/* ---------------------------------------------------------------------------- */
 const Home = () => {
   return (
     <>
-      {
-        //aqui invocan todos los otros componentes
-      }
-      <Header/>
+      {/* Aquí invocamos todos los otros componentes */}
+      <Header />
+       <Gallery /> 
+
+      {/* <ChakraProvider theme={theme}>
+        <CSSReset />
+        <Box textAlign="center" fontSize="xl">
+          <Gallery />
+        </Box>
+      </ChakraProvider> */}
     </>
   );
 };
 
 export default Home;
+
